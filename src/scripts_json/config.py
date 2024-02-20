647caf8bf3bb85b6353d59411046ec7b50345e91
Alximest(P4) <76548058+Alximest@users.noreply.github.com>
2024-02-21 04:37:15 +0500

    config.py

--- src/scripts_json/config.py
+++ src/scripts_json/config.py
@@ -1,4 +1,4 @@
-url_internal = "https://vats381960.megapbx.ru/crmapi/v1/history/inner/json"
-url_external = "https://vats381960.megapbx.ru/crmapi/v1/history/json"
-api_key = "cc9f44bb-dbfd-40e4-a2ef-8cacd6dae77c"
-url_site = "https://24baza.ru/megafon_api"
+url_internal = #json
+url_external = #json
+api_key = 
+url_site = 
